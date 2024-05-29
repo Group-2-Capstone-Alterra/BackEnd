035bfb2bc85ac77a84b613c61a746b82c344af05
bagasalfaristo <bagas.a.putra31@gmail.com>
2024-05-29 22:49:24 +0700

    rev routers

--- app/routers/router.go
+++ app/routers/router.go
@@ -29,12 +29,11 @@ func InitRouter(e *echo.Echo, db *gorm.DB, s3 *s3.S3, s3Bucket string) {
 	adminService := _adminService.New(adminData, hashService)
 	adminHandlerAPI := _adminHandler.New(adminService)
 
-
 	//user
-	e.POST("/user/register", userHandlerAPI.Register)
-	e.POST("/user/login", userHandlerAPI.Login)
-	e.GET("/user/profile", userHandlerAPI.Profile, middlewares.JWTMiddleware())
-	e.PUT("/user", userHandlerAPI.UpdateUserById, middlewares.JWTMiddleware())
+	e.POST("/users/create", userHandlerAPI.Register)
+	e.POST("/users/login", userHandlerAPI.Login)
+	e.GET("/users/profile", userHandlerAPI.Profile, middlewares.JWTMiddleware())
+	e.PUT("/users/update", userHandlerAPI.UpdateUserById, middlewares.JWTMiddleware())
 
 	//admin
 	e.POST("/admin/register", adminHandlerAPI.Register)
