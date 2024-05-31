13f18cf9ead5fc875eae7301d7cb9b3c3fc3f5ac
bagasalfaristo <bagas.a.putra31@gmail.com>
2024-06-01 01:36:49 +0700

    sort by price

--- features/product/entity.go
+++ features/product/entity.go
@@ -19,7 +19,7 @@ type Core struct {
 
 type DataInterface interface {
 	Insert(input Core) error
-	SelectAll(offset uint) ([]Core, error)
+	SelectAll(offset uint, sortStr string) ([]Core, error)
 	SelectAllAdmin(userid uint, offset uint) ([]Core, error)
 	SelectById(id uint) (*Core, error)
 	SelectByIdAdmin(id uint, userid uint) (*Core, error)
@@ -29,7 +29,7 @@ type DataInterface interface {
 
 type ServiceInterface interface {
 	Create(id uint, input Core, file io.Reader, handlerFilename string) (string, error)
-	GetAll(userid uint, offset uint) ([]Core, error)
+	GetAll(userid uint, offset uint, sortStr string) ([]Core, error)
 	GetProductById(id uint, userid uint) (data *Core, err error)
 	UpdateById(id uint, userid uint, input Core) error
 	Delete(id uint, userid uint) error
