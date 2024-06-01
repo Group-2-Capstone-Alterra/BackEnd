49a09f012628f5979bd1683e2ccd6afa002abc11
bagasalfaristo <bagas.a.putra31@gmail.com>
2024-06-02 06:10:22 +0700

    rev feat concultation

--- features/consultation/data/model.go
+++ features/consultation/data/model.go
@@ -7,22 +7,22 @@ import (
 )
 
 type Consultation struct {
-    gorm.Model
-    UserID        uint   
-    DoctorID      uint   
-    Consultation  string 
-    Response      string 
-    Status        string 
+	gorm.Model
+	UserID       uint
+	DoctorID     uint
+	Consultation string
+	Response     string
+	Status       string
 }
 
-func (c *Consultation) ToCore() consultation.ConsultationCore {
-    return consultation.ConsultationCore{
-        ID:           c.ID,
-        UserID:       c.UserID,
-        DoctorID:     c.DoctorID,
-        Consultation: c.Consultation,
-        Response:     c.Response,
-        Status:       c.Status,
-        CreatedAt:    c.CreatedAt,
-    }
+func ToCore(c Consultation) consultation.ConsultationCore {
+	return consultation.ConsultationCore{
+		ID:           c.ID,
+		UserID:       c.UserID,
+		DoctorID:     c.DoctorID,
+		Consultation: c.Consultation,
+		Response:     c.Response,
+		Status:       c.Status,
+		CreatedAt:    c.CreatedAt,
+	}
 }
