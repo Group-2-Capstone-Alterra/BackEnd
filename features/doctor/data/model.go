fa9a66656f7a728cf32e4516e7f8ba887ebaf89e
bagasalfaristo <bagas.a.putra31@gmail.com>
2024-06-04 05:39:50 +0700

    add feat at doctor

--- features/doctor/data/model.go
+++ features/doctor/data/model.go
@@ -1,10 +1,50 @@
 package data
 
-import "gorm.io/gorm"
+import (
+	"PetPalApp/features/availdaydoctor"
+	"PetPalApp/features/doctor"
+
+	"gorm.io/gorm"
+)
 
 type Doctor struct {
 	gorm.Model
+	AdminID        uint
 	FullName       string
 	Email          string
 	Specialization string
+}
+
+type AvailableDay struct {
+	gorm.Model
+	DoctorID  uint
+	Monday    bool
+	Tuesday   bool
+	Wednesday bool
+	Thursday  bool
+	Friday    bool
+}
+
+func GormToCore(doctorGorm Doctor) doctor.Core {
+	result := doctor.Core{
+		ID:             doctorGorm.ID,
+		AdminID:        doctorGorm.AdminID,
+		FullName:       doctorGorm.FullName,
+		Email:          doctorGorm.Email,
+		Specialization: doctorGorm.Specialization,
+	}
+	return result
+}
+
+func AvailGormToCore(availGorm AvailableDay) availdaydoctor.Core {
+	result := availdaydoctor.Core{
+		ID:        availGorm.ID,
+		DoctorID:  availGorm.DoctorID,
+		Monday:    availGorm.Monday,
+		Tuesday:   availGorm.Tuesday,
+		Wednesday: availGorm.Wednesday,
+		Thursday:  availGorm.Thursday,
+		Friday:    availGorm.Friday,
+	}
+	return result
 }
--- features/doctor/data/query.go
+++ features/doctor/data/query.go
@@ -1,30 +1,81 @@
 package data
 
 import (
+	"PetPalApp/features/availdaydoctor"
 	"PetPalApp/features/doctor"
+	"log"
 
 	"gorm.io/gorm"
 )
 
 type DoctorModel struct {
-    db *gorm.DB
+	db *gorm.DB
 }
 
 func New(db *gorm.DB) doctor.DoctorModel {
-    return &DoctorModel{
-        db: db,
-    }
+	return &DoctorModel{
+		db: db,
+	}
 }
 
 func (dm *DoctorModel) AddDoctor(doctor doctor.Core) error {
-    doctorGorm := Doctor{
-        FullName:      doctor.FullName,
-        Email:         doctor.Email,
-        Specialization: doctor.Specialization,
-    }
-    tx := dm.db.Create(&doctorGorm)
-    if tx.Error != nil {
-        return tx.Error
-    }
-    return nil
+	doctorGorm := Doctor{
+		FullName:       doctor.FullName,
+		Email:          doctor.Email,
+		Specialization: doctor.Specialization,
+	}
+	tx := dm.db.Create(&doctorGorm)
+	if tx.Error != nil {
+		return tx.Error
+	}
+	return nil
+}
+
+func (dm *DoctorModel) SelectById(id uint) (*doctor.Core, error) {
+	log.Println("[Query Doctor - SelectById]")
+	var doctorData Doctor
+	log.Println("[Query Doctor - SelectById] AdminID", id)
+	tx := dm.db.Where("admin_id = ?", id).Find(&doctorData)
+	if tx.Error != nil {
+		return nil, tx.Error
+	}
+	var doctorCore = GormToCore(doctorData)
+	log.Println("[Query Doctor - SelectById] doctorCore", doctorCore)
+
+	return &doctorCore, nil
+}
+
+func (dm *DoctorModel) SelectAvailDayById(id uint) (*availdaydoctor.Core, error) {
+	var availDay AvailableDay
+	log.Println("[Query Doctor - SelectAvailDayById] iD Param", id)
+
+	tx := dm.db.Where("doctor_id = ?", id).Find(&availDay)
+	if tx.Error != nil {
+		return nil, tx.Error
+	}
+	var availDayCore = AvailGormToCore(availDay)
+	log.Println("[Query Doctor - SelectAvailDayById] availDayCore", availDayCore)
+
+	return &availDayCore, nil
+}
+
+func (dm *DoctorModel) SelectAllDoctor() ([]doctor.Core, error) {
+	var allDoctor []Doctor
+
+	tx := dm.db.Find(&allDoctor)
+	if tx.Error != nil {
+		return nil, tx.Error
+	}
+
+	var allDoctorCore []doctor.Core
+	for _, v := range allDoctor {
+		allDoctorCore = append(allDoctorCore, doctor.Core{
+			ID:             v.ID,
+			AdminID:        v.AdminID,
+			FullName:       v.FullName,
+			Email:          v.Email,
+			Specialization: v.Specialization,
+		})
+	}
+	return allDoctorCore, nil
 }
