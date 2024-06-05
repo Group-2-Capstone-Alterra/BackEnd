29fcb5b529ada98e3e3c68a73825c89e92a51a21
wfhadit <wfhadit@gmail.com>
2024-06-05 18:42:30 +0700

    fix : admin response

--- features/admin/handler/response.go
+++ features/admin/handler/response.go
@@ -1,9 +1,43 @@
 package handler
 
+import "PetPalApp/features/admin"
+
 type AdminResponse struct {
+	ID			   uint   `json:"id"`
 	FullName       string `json:"full_name"`
 	Email          string `json:"email"`
 	NumberPhone    string `json:"number_phone"`
 	Address        string `json:"address"`
 	ProfilePicture string `json:"profile_picture"`
+	Coordinate	   string `json:"coordinate"`
+}
+
+type LoginResponse struct {
+	ID			   uint   `json:"id"`
+	FullName       string `json:"full_name"`
+	Email          string `json:"email"`
+	Token		   string `json:"token"`
+}
+
+func ResponseProfile(adminResponse admin.Core) AdminResponse {
+	result := AdminResponse{
+		ID:             adminResponse.ID,
+		FullName:       adminResponse.FullName,
+		Email:          adminResponse.Email,
+		NumberPhone:    adminResponse.NumberPhone,
+		Address:        adminResponse.Address,
+		ProfilePicture: adminResponse.ProfilePicture,
+		Coordinate:     adminResponse.Coordinate,
+	}
+	return result
+}
+
+func ResponseLogin(adminResponse *admin.Core) LoginResponse {
+	result := LoginResponse{
+		ID:       adminResponse.ID,
+		FullName: adminResponse.FullName,
+		Email: 	  adminResponse.Email,
+		Token:    adminResponse.Token,
+	}
+	return result
 }
