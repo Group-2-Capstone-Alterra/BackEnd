00d250c4ce672ef818f91de914e606a5a1049d30
bagasalfaristo <bagas.a.putra31@gmail.com>
2024-06-05 10:33:49 +0700

    rev

--- app/routers/router.go
+++ app/routers/router.go
@@ -61,7 +61,7 @@ func InitRouter(e *echo.Echo, db *gorm.DB, s3 *s3.S3, s3Bucket string) {
 	productHandlerAPI := _productHandler.New(productService, helperService)
 
 	doctorData := _doctorData.New(db)
-	doctorService := _doctorService.New(doctorData)
+	doctorService := _doctorService.New(doctorData, helperService)
 	doctorHandlerAPI := _doctorHandler.New(doctorService)
 
 	adminData := _adminData.New(db)
