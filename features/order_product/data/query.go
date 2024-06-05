289590c1ee5e1b11d59e96e3f4965206ece161fc
wfhadit <wfhadit@gmail.com>
2024-06-05 09:11:46 +0700

    feat : get order products

--- features/order_product/data/query.go
+++ features/order_product/data/query.go
@@ -26,3 +26,18 @@ func (opm *OrderProductModel) CreateOrderProduct(opCore order_product.OrderProdu
 	tx := opm.db.Create(&op)
 	return tx.Error
 }
+
+func (opm *OrderProductModel) GetOrderProductsByOrderID(orderID uint) ([]order_product.OrderProductCore, error) {
+	var orderProducts []OrderProduct
+	tx := opm.db.Where("order_id = ?", orderID).Find(&orderProducts)
+	if tx.Error != nil {
+		return nil, tx.Error
+	}
+
+	var result []order_product.OrderProductCore
+	for _, op := range orderProducts {
+		result = append(result, ToCore(op))
+	}
+
+	return result, nil
+}
--- features/order_product/handler/handler.go
+++ features/order_product/handler/handler.go
@@ -5,6 +5,7 @@ import (
 	"PetPalApp/features/order_product"
 	"PetPalApp/utils/responses"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
@@ -31,4 +32,18 @@ func (oph *OrderProductHandler) CreateOrderProduct(c echo.Context) error {
 	}
 
 	return c.JSON(http.StatusCreated, responses.JSONWebResponse("Order product created successfully", nil))
+}
+
+func (oph *OrderProductHandler) GetOrderProductsByOrderID(c echo.Context) error {
+	orderID, err := strconv.Atoi(c.Param("order_id"))
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, responses.JSONWebResponse("Invalid order ID", nil))
+	}
+
+	orderProducts, err := oph.OrderProductService.GetOrderProductsByOrderID(uint(orderID))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, responses.JSONWebResponse("Failed to get order products", nil))
+	}
+
+	return c.JSON(http.StatusOK, responses.JSONWebResponse("Order products retrieved successfully", orderProducts))
 }
