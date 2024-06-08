202fd9bfeb60fccfafa780e2ddf15e1aa37506a8
bagasalfaristo <bagas.a.putra31@gmail.com>
2024-06-09 06:01:02 +0700

    update chat response

--- features/chat/handler/response.go
+++ features/chat/handler/response.go
@@ -8,46 +8,68 @@ import (
 )
 
 type ChatResponse struct {
-	ID                  uint      `json:"id"`
-	ConsultationID      uint      `json:"roomchat_id"`
-	SenderID            uint      `json:"sender_id"`
-	SenderName          string    `json:"sender_name"`
-	SenderProfilePict   string    `json:"sender_profilepict"`
-	ReceiverID          uint      `json:"receiver_id"`
-	ReceiverName        string    `json:"receiver_name"`
-	ReceiverProfilePict string    `json:"receiver_profilepict"`
-	Message             string    `json:"message"`
-	TimeStamp           time.Time `json:"time_stamp"`
+	ID             uint                 `json:"id"`
+	ConsultationID uint                 `json:"roomchat_id"`
+	Sender         SenderInformations   `json:"sender"`
+	Receiver       ReceiverInformations `json:"receiver"`
+	Message        string               `json:"message"`
+	TimeStamp      time.Time            `json:"time_stamp"`
+}
+
+type SenderInformations struct {
+	ID             uint   `json:"id"`
+	Role           string `json:"role"`
+	FullName       string `json:"full_name"`
+	ProfilePicture string `json:"profile_picture"`
+}
+
+type ReceiverInformations struct {
+	ID             uint   `json:"id"`
+	Role           string `json:"role"`
+	FullName       string `json:"full_name"`
+	ProfilePicture string `json:"profile_picture"`
 }
 
 func AllResponseChatFromUser(gorm chat.ChatCore, user user.Core, doctor doctor.Core) ChatResponse {
 	result := ChatResponse{
-		ID:                  gorm.ID,
-		ConsultationID:      gorm.ConsultationID,
-		SenderID:            gorm.SenderID,
-		SenderName:          user.FullName,
-		SenderProfilePict:   user.ProfilePicture,
-		ReceiverID:          doctor.AdminID,
-		ReceiverName:        doctor.FullName,
-		ReceiverProfilePict: doctor.ProfilePicture,
-		Message:             gorm.Message,
-		TimeStamp:           gorm.TimeStamp,
+		ID:             gorm.ID,
+		ConsultationID: gorm.ConsultationID,
+		Sender: SenderInformations{
+			ID:             gorm.SenderID,
+			Role:           user.Role,
+			FullName:       user.FullName,
+			ProfilePicture: user.ProfilePicture,
+		},
+		Receiver: ReceiverInformations{
+			ID:             doctor.AdminID,
+			Role:           "doctor",
+			FullName:       doctor.FullName,
+			ProfilePicture: gorm.ReceiverName,
+		},
+		Message:   gorm.Message,
+		TimeStamp: gorm.TimeStamp,
 	}
 	return result
 }
 
 func AllResponseChatFromDoctor(gorm chat.ChatCore, user user.Core, doctor doctor.Core) ChatResponse {
 	result := ChatResponse{
-		ID:                  gorm.ID,
-		ConsultationID:      gorm.ConsultationID,
-		SenderID:            doctor.AdminID,
-		SenderName:          doctor.FullName,
-		SenderProfilePict:   doctor.ProfilePicture,
-		ReceiverID:          gorm.ReceiverID,
-		ReceiverName:        user.FullName,
-		ReceiverProfilePict: user.ProfilePicture,
-		Message:             gorm.Message,
-		TimeStamp:           gorm.TimeStamp,
+		ID:             gorm.ID,
+		ConsultationID: gorm.ConsultationID,
+		Sender: SenderInformations{
+			ID:             doctor.AdminID,
+			Role:           "doctor",
+			FullName:       doctor.FullName,
+			ProfilePicture: doctor.ProfilePicture,
+		},
+		Receiver: ReceiverInformations{
+			ID:             gorm.ReceiverID,
+			Role:           user.Role,
+			FullName:       user.FullName,
+			ProfilePicture: user.ProfilePicture,
+		},
+		Message:   gorm.Message,
+		TimeStamp: gorm.TimeStamp,
 	}
 	return result
 }
